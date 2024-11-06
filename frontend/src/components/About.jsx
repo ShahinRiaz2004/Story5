885d717c67719549e9fe4b4e01b7649f482b874f
Shahin Riaz <shahinriaz2004@gmail.com>
2024-11-06 22:32:08 +0430

    add files

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+            We pride ourselves on bringing the best coffee to the heart of Ahangama. 
+             We also offer a daily assortment of fresh baked goods and our Award Winning Chocolate Chip Cookies. 
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Sundays Cafe & Restaurant</div>
+          <div className="right">
+            <p>Goviyapana, Ahangama.</p>
+            <p>Open: 08:00 AM - 5:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By NewGenius</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By JanithNanayakkara.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
