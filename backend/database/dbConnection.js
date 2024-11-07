bd1c9246de0332acda008d19eb7fc97b423f092a
FH <130464629+ShahinRiaz2004@users.noreply.github.com>
2024-11-07 14:53:54 +0430

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect("mongodb://localhost:27017/restaurant")
+    .connect("mongodb+srv://habibifarhangiz:123231213@cluster0.jjx0o.mongodb.net/?retryWrites=true&w=majority&appName=restaurant")
     .then(() => {
       console.log("Connected to database!");
     })
