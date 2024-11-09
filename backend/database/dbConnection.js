f64f48e9e1cdf719a06397922286d4aab0531bf5
FH <130464629+ShahinRiaz2004@users.noreply.github.com>
2024-11-09 18:05:50 +0430

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
+  const connectionString = "mongodb+srv://hamoon:123231213@cluster1.y9abtdr.mongodb.net/";
+
   mongoose
-    .connect("mongodb+srv://hamoon:123231213@cluster1.y9abtdr.mongodb.net/")
+    .connect(connectionString)
     .then(() => {
       console.log("Connected to database!");
     })
     .catch((err) => {
-      console.log(`Some error occured while connecing to database: ${err}`);
+      console.log(`Some error occurred while connecting to the database: ${err}`);
     });
 };
